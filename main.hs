6be369c09c8eae2a9fee97e33a9ba691ffbc7048
Adam Vogt <vogt.adam@gmail.com>
2026-06-05 13:24:33 -0400

    remove unused package

--- main.hs
+++ main.hs
@@ -8,7 +8,6 @@ import qualified Data.ByteString.Char8 as B8
 import Data.Char
 import Data.Foldable
 import Data.IORef
-import Data.List.Split
 import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
